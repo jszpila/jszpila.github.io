6dc7a4c304b6ea1feb322bd09973af5219ddbd42
Jan Szpila <jszpila@gmail.com>
2018-07-16 22:40:17 -0500

    Add lightbox loading message

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,8 @@
 (function () {
   $(function () {
+    $(this).ekkoLightbox({
+      loadingMessage: 'Loading all the things...',
+    });
     $(document).on('click', '[data-toggle="lightbox"]', function (event) {
       event.preventDefault();
       $(this).ekkoLightbox();
