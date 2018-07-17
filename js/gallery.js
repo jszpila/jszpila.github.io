88fe96362efe7ed5a51e5c3b16b155cc95126011
Jan Szpila <jszpila@gmail.com>
2018-07-16 22:48:41 -0500

    Remove lightbox loading message

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,5 @@
 (function () {
   $(function () {
-    $(this).ekkoLightbox({
-      loadingMessage: 'Loading all the things...',
-    });
     $(document).on('click', '[data-toggle="lightbox"]', function (event) {
       event.preventDefault();
       $(this).ekkoLightbox();
